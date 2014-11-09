f26e193f099e562353a3ca45d3e6ac88d6a43d87
terroreek <terror.eek@gmail.com>
2014-11-08 21:54:01 -0500

    add link script

---
+++ linkconfigs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# link files
+ln -s Public/zpresto_configs/zlogin .zlogin 
+ln -s Public/zpresto_configs/zpreztorc .zpreztorc
+ln -s Public/zpresto_configs/zshenv .zshenv 
+ln -s Public/zpresto_configs/zlogout .zlogout
+ln -s Public/zpresto_configs/zprofile .zprofile
+ln -s Public/zpresto_configs/zshrc .zshrc 
+
